ab7049f590e282bae6633b29e2d55df5ebeff56c
Aline de Oliveira <107756464+AlineAMDO@users.noreply.github.com>
2022-12-09 16:35:22 -0300

    tarefa 2 operadores

---
+++ tarefa 12/codigo.js
@@ -0,0 +1,35 @@
+alert("A página acabou de ser carregada!");
+
+window.alert("Hoje a aula é sobre operadores")
+window.confirm("vamos começar?")
+var nome = window.prompt("Qual é o seu nome?")
+window.alert("É um prazer te conhecer " + nome + "!")
+var ano = window.prompt("Em que ano você nasceu?")
+let idade = 2022 - ano
+window.alert("Você tem: " + idade + " anos!")
+
+function funcao1()
+{alert("Você será redirecionado para a página do Google")};
+document.write("<br>");
+document.write("<br>");
+
+let numero1=4;
+let numero2=2;
+
+let soma=(numero1 + numero2);
+let subtracao=(numero1 - numero2);
+let divisao=(numero1 / numero2);
+let multiplicacao=(numero1 * numero2);
+
+
+document.write("A soma entre " + numero1 + " e " + numero2 + " é: " + soma + "<br>");
+document.write("A subtração entre " + numero1 + " e " + numero2 + " é: " + subtracao + "<br>");
+document.write("A divisão entre " + numero1 + " e " + numero2 + " é: " + divisao + "<br>");
+document.write("A multiplicação entre " + numero1 + " e " + numero2 + " é: " + multiplicacao + "<br>");
+
+document.write("<br>");
+document.write("<br>");
+
+(console.log(numero1 == numero2));
+(console.log(numero1 != numero2));
+
