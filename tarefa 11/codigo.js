681142c626648b902efc8b909440a21e9d2d9f4e
Aline de Oliveira <107756464+AlineAMDO@users.noreply.github.com>
2022-12-09 11:41:39 -0300

    javascript 1

---
+++ tarefa 11/codigo.js
@@ -0,0 +1,16 @@
+alert("Olá, Mundo!")
+
+document.write("Olá, Mundo!");
+document.write("<h1>Alô, Mundo</h1>");
+
+var descricao = "Esta é uma descrição";
+alert(descricao);
+
+function funcao1()
+{alert("Você será redirecionado para o Google")};
+
+
+var nome = "Aline";
+alert("Parabéns, " + nome + "! Você terminou a atividade 11.");
+
+ 
